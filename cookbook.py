7e0264ae0d439107bd8f934ce605e448507c9b12
codeartha <codeartha@gmail.com>
2019-09-05 15:54:44 +0200

    database management and initialization from the cookbook app

--- cookbook.py
+++ cookbook.py
@@ -35,6 +35,124 @@ def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
+def connect(sqlite_file):
+    """ Make connection to an SQLite database file """
+    try:
+        conn = sqlite3.connect(sqlite_file)
+        cur = conn.cursor()
+    except sqlite3.Error as e:
+        print('could not connect to database')
+        print(e)
+        conn.close()
+        return 0
+    else:
+        return conn, cur
+
+
+def close(conn):
+    """ Commit changes and close connection to the database """
+    conn.commit()
+    conn.close()
+    return 1
+
+
+def initialize_database():
+    if os.path.isfile(DATABASE_FILE):
+        clear_terminal()
+        print('Warning: The database file already exist. Going further will errase data.')
+        print('Options:')
+        print('1: Cancel, keep my data safe')
+        print('2: Rebuild just the inventory database')
+        print('3: Rebuild both inventory and recipes database')
+        action = int(input('What do you want to do [1, 2, 3]? '))
+        if action == 1:
+            print('Initialization cancelled...')
+            return 1
+        elif action == 2:
+            connection, cursor = connect(DATABASE_FILE)
+
+            cursor.execute("""
+                DROP TABLE IF EXISTS items
+            """)
+
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS items(
+                id INTEGER UNSIGNED PRIMARY KEY,
+                item TEXT UNIQUE NOT NULL,
+                quantity REAL)
+            """)
+
+            close(connection)
+            return 1
+        elif action == 3:
+            connection, cursor = connect(DATABASE_FILE)
+
+            cursor.execute("""DROP TABLE IF EXISTS items""")
+            cursor.execute("""DROP TABLE IF EXISTS recipes""")
+            cursor.execute("""DROP TABLE IF EXISTS ingredients""")
+
+            connection.commit()
+
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS items(
+                id INTEGER UNSIGNED PRIMARY KEY,
+                item TEXT UNIQUE NOT NULL,
+                quantity REAL)
+            """)
+
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS recipes(
+                id INTEGER UNSIGNED PRIMARY KEY,
+                title TEXT UNIQUE NOT NULL,
+                type TEXT CHECK(type IN ('starter', 'dish', 'dessert', 'snack')) NOT NULL DEFAULT 'dish',
+                description TEXT,
+                instructions TEXT)
+            """)
+
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS ingredients(
+                id INTEGER UNSIGNED PRIMARY KEY,
+                quantity REAL,
+                item_id INTEGER UNSIGNED,
+                recipe_id INTEGER UNSIGNED)
+            """)
+
+            close(connection)
+            return 1
+        else:
+            print('Initialization cancelled...')
+            return 1
+    else:
+        print('Initializing a new empty database to story your inventory and crafting recipes')
+        connection, cursor = connect(DATABASE_FILE)
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS items(
+            id INTEGER UNSIGNED PRIMARY KEY,
+            item TEXT UNIQUE NOT NULL,
+            quantity REAL)
+        """)
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS recipes(
+            id INTEGER UNSIGNED PRIMARY KEY,
+            title TEXT UNIQUE NOT NULL,
+            type TEXT CHECK(type IN ('starter', 'dish', 'dessert', 'snack')) NOT NULL DEFAULT 'dish',
+            description TEXT,
+            instructions TEXT)
+        """)
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS ingredients(
+            id INTEGER UNSIGNED PRIMARY KEY,
+            quantity REAL,
+            item_id INTEGER UNSIGNED NOT NULL,
+            recipe_id INTEGER UNSIGNED NOT NULL)
+        """)
+
+        close(connection)
+    return 1
+
 def main(args):
     """ Parsing command line arguments """
     if args[1] == 'new':
